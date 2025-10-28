b10989885ad35594acfa7fd7546dd400a1ab51af
Abdul-khalid-2 <abdulkhalidmasood@domain.com>
2025-10-28 22:51:46 +0500

    Reset Link error handling changes

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -8,6 +8,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
 use Illuminate\View\View;
 
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\Exception\UnexpectedResponseException;
+
 class PasswordResetLinkController extends Controller
 {
     /**
@@ -29,16 +32,23 @@ class PasswordResetLinkController extends Controller
             'email' => ['required', 'email'],
         ]);
 
-        // We will send the password reset link to this user. Once we have attempted
-        // to send the link, we will examine the response then see the message we
-        // need to show to the user. Finally, we'll send out a proper response.
-        $status = Password::sendResetLink(
-            $request->only('email')
-        );
+        try {
+            $status = Password::sendResetLink(
+                $request->only('email')
+            );
 
-        return $status == Password::RESET_LINK_SENT
-                    ? back()->with('status', __($status))
-                    : back()->withInput($request->only('email'))
-                        ->withErrors(['email' => __($status)]);
+            return $status == Password::RESET_LINK_SENT
+                ? back()->with('status', __($status))
+                : back()->withInput($request->only('email'))
+                ->withErrors(['email' => __($status)]);
+        } catch (UnexpectedResponseException | TransportExceptionInterface $e) {
+            // This catches "550 No such recipient" and similar SMTP issues
+            return back()->withInput($request->only('email'))
+                ->withErrors(['email' => 'This email address could not be reached. Please check and try again.']);
+        } catch (\Exception $e) {
+            // Generic fallback for any other unforeseen error
+            return back()->withInput($request->only('email'))
+                ->withErrors(['email' => 'Something went wrong while sending the reset link. Please try again later.']);
+        }
     }
 }
