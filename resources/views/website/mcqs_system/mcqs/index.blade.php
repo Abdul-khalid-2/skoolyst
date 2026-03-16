013918af3aabc16801d128fcbdaf70b4e22898ac
Abdul-khalid-2 <abdulkhalidmasood@domain.com>
2026-03-16 11:38:18 +0500

    practice mcqs section commente outed for now

--- resources/views/website/mcqs_system/mcqs/index.blade.php
+++ resources/views/website/mcqs_system/mcqs/index.blade.php
@@ -253,8 +253,9 @@
         </div>
 
         <div class="row">
+            comming soon
             @foreach($recentMcqs as $mcq)
-            <div class="col-md-6">
+            <!-- <div class="col-md-6">
                 <div class="mcq-card">
                     <div class="d-flex justify-content-between align-items-start mb-3">
                         <span class="mcq-difficulty difficulty-{{ $mcq->difficulty_level }}">
@@ -290,7 +291,7 @@
                         </a>
                     </div>
                 </div>
-            </div>
+            </div> -->
             @endforeach
         </div>
 
