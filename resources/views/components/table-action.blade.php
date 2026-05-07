b05ea590c4d38601bf8c497407d984606b3650eb
Abdul-khalid <abdulkhalidmasood@gmail.com>
2026-05-07 09:37:40 +0500

    action button dorpdown layout is now on top

--- resources/views/components/table-action.blade.php
+++ resources/views/components/table-action.blade.php
@@ -5,13 +5,13 @@
         type="button"
         class="btn btn-sm btn-outline-secondary"
         data-bs-toggle="dropdown"
-        data-bs-display="static"
+        data-bs-popper-config='{"strategy":"fixed"}'
         aria-expanded="false"
         aria-label="{{ __('Actions') }}"
     >
         <i class="fas fa-ellipsis-v" aria-hidden="true"></i>
     </button>
-    <ul class="dropdown-menu dropdown-menu-end shadow-sm">
+    <ul class="dropdown-menu dropdown-menu-end shadow-sm" style="z-index: 1080;">
         {{ $slot }}
     </ul>
 </div>
