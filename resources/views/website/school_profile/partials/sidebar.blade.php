cd272554b9266ecfc977d26ae5a9b8f7582e6e5a
Abdul-khalid <abdulkhalidmasood@gmail.com>
2026-06-09 15:48:18 +0500

    condition fixing when no social media account line fine

--- resources/views/website/school_profile/partials/sidebar.blade.php
+++ resources/views/website/school_profile/partials/sidebar.blade.php
@@ -116,11 +116,13 @@
                     @endif
                 @endforeach
             @else
-                <a href="#" class="social-link"><i class="fab fa-facebook-f"></i></a>
+
+                <p class="no-content">No social media links available.</p>
+                <!-- <a href="#" class="social-link"><i class="fab fa-facebook-f"></i></a>
                 <a href="#" class="social-link"><i class="fab fa-twitter"></i></a>
                 <a href="#" class="social-link"><i class="fab fa-instagram"></i></a>
                 <a href="#" class="social-link"><i class="fab fa-linkedin-in"></i></a>
-                <a href="#" class="social-link"><i class="fab fa-youtube"></i></a>
+                <a href="#" class="social-link"><i class="fab fa-youtube"></i></a> -->
             @endif
         </div>
     </section>
